6b4969ca05414925421f37a9a3f81381e755bbc3
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-08 18:12:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort((a,b)=>Number(a)-Number(b));
+	while(arr.length>1){
+		let first=arr.shift();
+		let second=arr.shift();
+		arr.push(first+second);
+		arr.sort((a,b)=>Number(a)-Number(b));
+	}
+	  return arr[0];
 }
-
 module.exports=mincost;
